eced9ac645d2fc760b1b93f4d8b51542b0fe8410
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-12-18 10:05:25 +0700

    Add answer for make_bricks()

--- logic-2/make_brick.py
+++ logic-2/make_brick.py
@@ -1,4 +1,19 @@
-def make_brick(small, big, goal):
-  return False
+def make_bricks(small, big, goal):
+    max_big = big * 5
+    remainder = 0
 
-print(make_brick(3, 1, 8))
+    if goal <= max_big:
+        remainder = goal % 5
+    else:
+        remainder = goal - max_big
+
+    if remainder <= small:
+        return True
+
+    return False
+
+
+print(make_bricks(3, 1, 9))
+
+# print(make_bricks(3, 1, 8))
+# print(make_bricks(3, 2, 10))
